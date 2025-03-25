500f24ed2e9a87f8a1c8e064b4d0c085bc649cc2
RaccoonRyu <rjhwind94@gmail.com>
2025-03-25 09:54:06 +0900

    스태틱 메서드, 와일드 카드 연습문제 작성 (완)

--- src/Java_Post_Advanced2/CH01_Generic/test/ex3/unit/UnitPrinter.java
+++ src/Java_Post_Advanced2/CH01_Generic/test/ex3/unit/UnitPrinter.java
@@ -1,10 +1,15 @@
 package Java_Post_Advanced2.CH01_Generic.test.ex3.unit;
 
 public class UnitPrinter {
-
-    public static void printV1(Shuttle<Marine> shuttle1) {
+    // 제네릭 메서드
+    public static <T extends BioUnit> void printV1(Shuttle<T> t1) {
+        T unit = t1.out();
+        System.out.println("이름 : " + unit.getName() + ", HP : " + unit.getHp());
     }
 
-    public static void printV2(Shuttle<Marine> shuttle1) {
+    // 와일드 카듬
+    public static void printV2(Shuttle<? extends BioUnit> t1) {
+        BioUnit unit = t1.out();
+        System.out.println("이름 : " + unit.getName() + ", HP : " + unit.getHp());
     }
 }
